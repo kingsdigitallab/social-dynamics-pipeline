7c2cdb5304980d5e0aaeb0e011f6ba3f3927ca8c
Mary Chester-Kadwell <mchesterkadwell@users.noreply.github.com>
2025-05-14 14:27:46 +0100

    fix: Database needs filepath for creation

--- pipeline/ui/config.py
+++ pipeline/ui/config.py
@@ -8,6 +8,9 @@ class Settings(BaseSettings):
     images_url_base: Path = Path("/images")
     demo_mode: bool = True
 
+    project_root: Path = Path(__file__).resolve().parents[2]
+    database_path: Path = project_root / "socdyn_db.db"
+
     class Config:
         env_file = ".env"
 
